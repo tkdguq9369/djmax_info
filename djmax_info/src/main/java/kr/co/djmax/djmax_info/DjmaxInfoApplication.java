f546c10243ec2509a2761ff032be8f66e42e24f1
kim <kim@59.1.175.103>
2020-02-08 20:53:06 +0900

    프로젝트 초기 설정 및 수정 ( 템플릿 )

---
+++ djmax_info/src/main/java/kr/co/djmax/djmax_info/DjmaxInfoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.djmax.djmax_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjmaxInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjmaxInfoApplication.class, args);
+	}
+
+}
---
+++ djmax_info/src/test/java/kr/co/djmax/djmax_info/DjmaxInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.djmax.djmax_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjmaxInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
